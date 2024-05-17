bf8187137926e5a2754e9347d79d708d7e455340
eunwha27 <eunwha27@bergen.org>
2024-05-17 12:48:34 -0400

    made balloon actor, changed turn degrees

--- balloon.java
+++ balloon.java
@@ -1,22 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class bee here.
+ * Write a description of class balloon here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class bee extends Actor
+public class balloon extends Actor
 {
     /**
-     * Act - do whatever the bee wants to do. This method is called whenever
+     * Act - do whatever the balloon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        move(5);
-        if(isAtEdge()){
-            turn(20);
-        }
+        // Add your action code here.
     }
 }
